c5f23836213f913c19cabd6566036de906db8417
Abhi Jain <devabhixda@gmail.com>
2020-10-30 21:30:31 +0530

    UI enhancements to help section

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             title: Text('Help'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.navigation),
+            icon: Icon(Icons.near_me),
             title: Text('Around'),
           ),
           BottomNavigationBarItem(
